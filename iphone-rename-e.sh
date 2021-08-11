cb169bbc90104f0c88f1e55f2099480a51d568c6
Suan Yong <suan.yong@gmail.com>
2021-08-10 22:00:41 -0400

    iphone-rename-e.sh

---
+++ iphone-rename-e.sh
@@ -0,0 +1,18 @@
+for X in IMG_E????.JPG
+do if [ ! -e "$X" ]
+   then echo "Does not exit: $X"
+        break
+   fi
+   REN=`echo $X | sed -e s/E// -e s/\.JPG/e.jpg/`
+   echo mv -i "$X" "$REN"
+        mv -i "$X" "$REN"
+done
+for X in IMG_E????.MOV
+do if [ ! -e "$X" ]
+   then echo "Does not exit: $X"
+        break
+   fi
+   REN=`echo $X | sed -e s/E// -e s/\.MOV/e.mov/`
+   echo mv -i "$X" "$REN"
+        mv -i "$X" "$REN"
+done
