abf4b5e9095f4c5e3bcccf8a387ee1b953eb672d
Suan Yong <suan.yong@gmail.com>
2023-04-07 16:00:20 -0400

    Thermapp adjustment use PyAV instead of OpenCV

---
+++ thermapp-video-adjust-av.py
@@ -0,0 +1,88 @@
+import av, os, sys  # pip3 install av
+
+# TODO TODO TODO TODO TODO TODO TODO TODO
+#
+# This PyAV version seems to work pretty well.
+#
+# TODO 1: Copy audio stream also
+# TODO 2: Although I set the output bit_rate to the same as input, the
+#         actual output bit_rate is slightly lower, though still better
+#         than default. Figure out how to fix this.
+#
+# TODO TODO TODO TODO TODO TODO TODO TODO
+
+WIDTH = 384
+HEIGHT = 288
+BADCOL = 163
+
+def image_edit_frame(img):
+    for y in range(0, HEIGHT-1):
+        img.putpixel((BADCOL, y), img.getpixel((BADCOL, y+1)))
+    return img
+
+def video_edit_frames(infile, outfile):
+    in_av = av.open(infile)
+    out_av = av.open(outfile, 'w')
+
+    in_stream = in_av.streams.video[0]
+
+    """
+    NOTE: Using template=in_stream is not working (probably meant to be used
+    for re-muxing and not for re-encoding).
+    """
+    #out_stream = out_av.add_stream(template=in_stream)
+
+    codec_name = in_stream.codec_context.name
+    # Of several ways to get frame rate, this one works for TAPlus output
+    fps = in_stream.average_rate
+    out_stream = out_av.add_stream(codec_name, fps)
+    out_stream.width = in_stream.codec_context.width
+    out_stream.height = in_stream.codec_context.height
+    out_stream.pix_fmt = in_stream.codec_context.pix_fmt
+    # This does change output bit rate from default, but doesn't match input
+    out_stream.bit_rate = in_stream.codec_context.bit_rate
+
+    print("width", out_stream.width)
+    print("height", out_stream.height)
+    print("pix_fmt", out_stream.pix_fmt)
+    print("bit_rate", out_stream.bit_rate)
+
+    " DOES NOT WORK: select low crf for high quality (but larger file size)."
+    #out_stream.options = {'crf': '24'}
+
+
+    for frame in in_av.decode(in_stream):
+        in_img = frame.to_image()
+        out_img = image_edit_frame(in_img)
+        # Note: to_image and from_image not required in this specific example.
+        out_frame = av.VideoFrame.from_image(out_img)
+        out_packet = out_stream.encode(out_frame)  # Encode video frame
+        # "Mux" the encoded frame (add the encoded frame to MP4 file).
+        out_av.mux(out_packet)
+
+    # Flush the encoder
+    out_packet = out_stream.encode(None)
+    out_av.mux(out_packet)
+
+    in_av.close()
+    out_av.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+    elif len(sys.argv) == 2:
+        infile = sys.argv[1]
+        base, ext = os.path.splitext(infile)
+        outfile = base + "-avadj" + ext
+    else:
+        print("USAGE: thermapp-video-adjust infile outfile")
+        sys.exit(1)
+    if not os.path.exists(infile):
+        print("ERROR: infile does not exist:", infile)
+        sys.exit(1)
+    if os.path.exists(outfile):
+        print("WARNING: outfile exists:", outfile)
+        if not input("Overwrite? [y/N]") in ('y', 'Y'):
+            sys.exit(1)
+    video_edit_frames(infile, outfile)
